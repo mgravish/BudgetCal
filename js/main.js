5ab0ea1035d95c80f15723a946d1a737b34b5611
Matt <mgravish@gmail.com>
2018-05-03 21:44:04 -0700

    asdf

--- js/main.js
+++ js/main.js
@@ -1,50 +1,38 @@
-//JS is used only to generate the the calendar dates dynamically and not to create the layout or styles.
+var now                 = new Date();
+var currentMonth        = now.getMonth()+1;
+var month               = now.getMonth();
+var currentYear         = now.getFullYear();
+var daysInMonthCount    = new Date(currentYear, currentMonth, 0).getDate();
+var date                = new Date();
+var monthNames          = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var currentMonthName    = (monthNames[month]);
+var firstDay            = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+var lastDay             = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+var calendarSpan        = 'calendar-span-' + firstDay;
+var header              = $('.calendar-header')[0];
 
-var now              = new Date();
-var currentMonth     = now.getMonth()+1;
-var month            = now.getMonth();
-var currentYear      = now.getFullYear();
-var daysInMonthCount = new Date(currentYear, currentMonth, 0).getDate();
 
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var currentMonthName = (monthNames[month]);
-
-// Getting the day of the week the month starts
-var firstDayOfMonth = new Date(currentYear + "-" + currentMonth + '-01').getDay();
-
-// Creating a variable to attach the firstDayOfMonth variable to the calendar-span class that will take advantage of the flex-basis code
-var calendarSpan = 'calendar-span-' + firstDayOfMonth;
-
-// Adding the calendar header with the current month and year
-var header = document.getElementById('calendar-header');
 header.innerHTML = header.innerHTML + currentMonthName + " " + currentYear;
 
-// Loop through all days in the month
-for (i = 1; i <= daysInMonthCount; i++) {
-  
-  // Making a variable with the HTML code for all days in the month
-  // I will insert the day in number to the span with class of number
-    var dayCalendarBlock = '<span class="day-block">'+
-                              '<span class="number">' + i + '</span>'+
-                            '</span>';
-    // Making a variable for the fist day day-block with the HTML code. 
-    // I also have to include the calendarSpan variable that will move the day-block to the corresponding day in the calendar
-    var firstDayofCalendarBlock = '<span class="day-block ' + calendarSpan + '">' +
-                                    '<span class="day-block-inner">' +
-                                      '<span class="number">' + i + '</span>'+
-                                    '</span>' +
-                                   '</span>';
-  
-    // Appending the firstDayOfCalendar variable to the first of the month
-    if ( i === 1)  {
-      var firstDayOfCalendar = document.getElementById('days-of-month');
-      firstDayOfCalendar.insertAdjacentHTML('beforeend', firstDayofCalendarBlock);
-    } else {
-      // Appending the daysOfCalendar variable to the days of the month
-      var dayOfCalendar = document.getElementById('days-of-month');
-      dayOfCalendar.insertAdjacentHTML('beforeend', dayCalendarBlock);
+var acct = $('.accounts')[0];
+$(acct).on('click',function() {
+    var target = $('#14')[0];
+    $(target).append('<div class="target"><div class="target-desc">test</div><div class="target-val">$23.00</div></div>');
+    console.log('clicked');
+});
+
+buildCalendar();
+function buildCalendar() {
+    for (i = 1; i <= 42; i++) {
+        var dayCalendarBlock = '<div class="day-block" id="'+i+'"><span class="number">'+(i-firstDay)+'</span></div>';
+        var firstDayofCalendarBlock = '<span class="non-month-day day-block"></span>';
+        if ( i <= firstDay || i > (firstDay+lastDay)) {
+          var firstDayOfCalendar = document.getElementById('days-of-month');
+          firstDayOfCalendar.insertAdjacentHTML('beforeend', firstDayofCalendarBlock);
+        } else {
+          var dayOfCalendar = document.getElementById('days-of-month');
+          dayOfCalendar.insertAdjacentHTML('beforeend', dayCalendarBlock);
+        }
     }
-};
-
-
+}
